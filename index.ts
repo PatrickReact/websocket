317756a8731fda7f537b89353cc622b661743266
Patrick <simonettipatrick88@gmail.com>
2025-06-23 07:55:27 +0200

    .

--- index.ts
+++ index.ts
@@ -4,7 +4,3 @@ import _ from "./types/global";
 import { env, runtime } from "@/lib/utils";
 
 const { default: server } = require(`./src/server/socket.${runtime}.ts`);
-
-server.listen(env.PORT ?? 8080, () => {
-	console.log(`Server listening on port ${env.PORT ?? 8080}`);
-});
